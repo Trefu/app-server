5eb82bcaab8baefdc95d8bb035c679344dfe9409
Lucas Leonel Lopez <tre.lopez@hotmail.com>
2021-07-14 04:58:51 -0300

    changes for heroku

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-    res.send("respuesta del servidor");
+    res.sendFile(__dirname + '/public/index.html')
 });
 
-app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + '/routes/login/index.html')
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(__dirname + '/routes/register/index.html')
+})
+
+app.use((req, res, next) => {
+    res.status(404).send("404")
+})
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
 });
+
